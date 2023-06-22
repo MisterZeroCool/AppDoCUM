fdf1b2313cba8af4e329a5b34925393d9f4208a9
Alexander Gavrilyuk <one1970zero@mail.ru>
2023-06-22 23:55:32 +0300

    Initial commit

---
+++ src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,9 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoint = 5
+    experiencePoint += 5
+    println(experiencePoint)
+    println(playerName)
+}
